bf25753f58b2c6f2ac90cc60d7cef9b9d7e46fa1
Alexey Golubev <dr.freecx@gmail.com>
2016-02-27 11:16:44 +0300

    [upd] test

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 pub mod structs;
 pub mod linalg;
+use linalg::Vec2;
 
 fn main() {
-    println!("Hello rust!");
+    let a = Vec2::new(1.0, 2.0);
+    let b = Vec2::new(3.0, 4.0);
+    let c = a + b;
+    println!("Hello Vec2 = {}", c);
 }
