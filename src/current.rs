381a2004f050f0b87f65cfd6361643cb0c64d227
Vova Abdrakhmanov <369565@gmail.com>
2016-02-28 15:11:47 +0300

    [fix] reduce scope of curr

--- src/current.rs
+++ src/current.rs
@@ -9,5 +9,7 @@ fn main() {
     let conf = Ini::load_from_file("config.ini").unwrap();
     let files = Files::from_config(&conf);
     let bzone = Bzone::from_config(&conf);
+    let fields = Field::from_config(&conf);
+    let plot = Plot::from_config(&conf);
     let _ = files.read_probabilities();
 }
