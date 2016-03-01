f9ff625d9defd97620e14d9764da896aef4abb33
Vova Abdrakhmanov <369565@gmail.com>
2016-03-01 11:08:12 +0300

    [upd + doc] (scattering) material

--- scattering/src/lib.rs
+++ scattering/src/lib.rs
@@ -1,7 +1,7 @@
 extern crate linalg;
 #[macro_use]
 // pub mod structs;
-// pub mod material;
+pub mod material;
 // pub mod modelling;
 // pub mod stats;
 mod rng;
--- scattering/src/material.rs
+++ scattering/src/material.rs
@@ -1,47 +1,50 @@
 use linalg::{Vec2, Point, Cross, dual_basis};
 
 pub trait Material {
-    fn energy(p: &Point) -> f64;
-
-    fn energy_polar(p: f64, theta: f64) -> f64 {
-        self.energy(Vec2::from_polar(p, theta))
+    /// Energy spectrum of electrons
+    fn energy(&self, p: &Point) -> f64;
+    /// Energy spectrum in polar coordinates
+    fn energy_polar(&self, p: f64, theta: f64) -> f64 {
+        self.energy(&Point::from_polar(p, theta))
     }
+    /// Gradient of energy in momentum space
+    fn energy_gradient(&self, p: &Point) -> Vec2;
 
-    fn energy_gradient(p: &Point) -> Vec2;
-
-    fn velocity(p: &Point) -> Vec2;
-
-    fn min_energy() -> f64;
-
-    fn max_energy() -> f64;
-
-    fn momentums(energy: f64, theta: f64) -> Vec<Point>;
-
-    fn brillouin_zone() -> BrillouinZone;
+    fn velocity(&self, p: &Point) -> Vec2;
+    /// Minimum of energy in brillouin zone
+    fn min_energy(&self) -> f64;
+    /// Maximum of energy in brillouin zone
+    fn max_energy(&self) -> f64;
+    /// Solves equation energy_polar(p, theta) = energy
+    fn momentums(&self, energy: f64, theta: f64) -> Vec<Point>;
+    /// brillouin zone structure
+    fn brillouin_zone(&self) -> BrillouinZone;
 }
 
 #[derive(Clone)]
+/// Representation of tetragonal brillouin zone of 2D material.
+/// a, b, c, and d are vertices of tetragon.
 pub struct BrillouinZone {
-    pub A: Point,
-    pub B: Point,
-    pub C: Point,
-    pub D: Point,
+    pub a: Point,
+    pub b: Point,
+    pub c: Point,
+    pub d: Point,
     pub basis: (Vec2, Vec2),
     pub dual_basis: (Vec2, Vec2),
 }
 
 impl BrillouinZone {
-    pub fn new(A: Point, B: Point, D: Point) -> Bzone {
-        let b = B - A;
-        let d = D - A;
-        let C = A + b + d;
-        let basis = (b, d);
+    pub fn new(a: Point, b: Point, d: Point) -> BrillouinZone {
+        let v1 = b - a;
+        let v2 = d - a;
+        let c = a + v1 + v2;
+        let basis = (v1, v2);
 
-        Bzone {
-            A: A,
-            B: B,
-            C: C,
-            D: D,
+        BrillouinZone {
+            a: a,
+            b: b,
+            c: c,
+            d: d,
             basis: basis,
             dual_basis: dual_basis(basis),
         }
@@ -50,7 +53,7 @@ impl BrillouinZone {
     /// Returns equivalent momentum in first brillouin zone
     pub fn to_first_bz(&self, p: &Point) -> Point {
 
-        let pv = *p - self.A;
+        let pv = *p - self.a;
 
         let n1 = pv.dot(self.dual_basis.0).floor();
         let n2 = pv.dot(self.dual_basis.1).floor();
@@ -61,15 +64,15 @@ impl BrillouinZone {
     /// Calculates maximum value of momentum in direction $\theta$ in first brillouin zone
     pub fn pmax(&self, theta: f64) -> f64 {
 
-        let OA = self.A.position();
-        let OB = self.B.position();
-        let OC = self.C.position();
-        let OD = self.D.position();
+        let oa = self.a.position();
+        let ob = self.b.position();
+        let oc = self.c.position();
+        let od = self.d.position();
         let l = Vec2::from_polar(1.0, theta);
 
         let mut res = -1.0;
 
-        let vs = vec![OA, OB, OC, OD, OA];
+        let vs = vec![oa, ob, oc, od, oa];
         for i in 0..4 {
             let p = vs[i].cross(vs[i + 1]) / l.cross(vs[i + 1] - vs[i]);
 
