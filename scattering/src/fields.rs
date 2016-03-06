aa7aa2ef72bc823cf2366b77bc12c78c39a2a0db
Vova Abdrakhmanov <369565@gmail.com>
2016-03-06 18:24:45 +0300

    [fix] forgotten fields.rs

---
+++ scattering/src/fields.rs
@@ -0,0 +1,30 @@
+use linalg::Vec2;
+
+#[derive(Clone)]
+/// Electromagnetic fields
+pub struct Fields {
+    /// Amplitudes of constant, first and second wave electric fields
+    pub e: (Vec2, Vec2, Vec2),
+    /// Amplitudes of z-component constant, first and second wave magnetic fields
+    pub b: (f64, f64, f64),
+    /// Frequences of waves. First value is unused and exists for consistency
+    pub omega: (f64, f64, f64),
+    pub phi: f64,
+}
+
+impl Fields {
+    pub fn new(e: (Vec2, Vec2, Vec2), b: (f64, f64, f64), omega: (f64, f64), phi: f64) -> Fields {
+        Fields {
+            e: e,
+            b: b,
+            omega: (0.0, omega.0, omega.1),
+            phi: phi,
+        }
+    }
+    pub fn zero() -> Fields {
+        Fields::new((Vec2::zero(), Vec2::zero(), Vec2::zero()),
+                    (0.0, 0.0, 0.0),
+                    (0.0, 0.0),
+                    0.0)
+    }
+}
