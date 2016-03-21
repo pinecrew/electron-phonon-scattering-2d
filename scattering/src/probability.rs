8626ccaddd970447683facf3e9ddb40399fcbf51
Vova Abdrakhmanov <369565@gmail.com>
2016-03-21 21:07:27 +0300

    [fix] scattering probability

--- scattering/src/probability.rs
+++ scattering/src/probability.rs
@@ -14,16 +14,48 @@ pub fn probability<T: Material>(energy: f64, m: &T, error: f64) -> f64 {
     while (new - old).abs() / new > error && iters_left > 0 {
         old = new;
         new = 0.0;
+        let dtheta = 2.0 * PI / (n as f64);
 
-        let mut prev: Vec<Point> = Vec::new();
+        let mut prev: Vec<Point> = m.momentums(energy, 0f64);
 
-        for i in 0..n {
-            let theta = (i as f64) / (n as f64) * 2.0 * PI;
+        for i in 1..n + 1 {
+            let theta = (i as f64) * dtheta;
             let curr = m.momentums(energy, theta);
             let l = min(curr.len(), prev.len());
             for j in 0..l {
                 new += (curr[j] - prev[j]).len() / m.energy_gradient(&curr[j]).len();
             }
+
+            // find endpoints
+            for i in prev.len()..curr.len() {
+                let mut dtheta = dtheta;
+                let mut theta = theta;
+                while dtheta > 1e-9 {
+                    if m.momentums(energy, theta - dtheta).len() > i {
+                        theta -= dtheta;
+                    } else {
+                        dtheta /= 2.0;
+                    }
+                }
+                new += (curr[i] - m.momentums(energy, theta)[i]).len() /
+                       m.energy_gradient(&curr[i]).len();
+            }
+
+
+            for i in curr.len()..prev.len() {
+                let mut dtheta = dtheta;
+                let mut theta = theta - dtheta;
+                while dtheta > 1e-9 {
+                    if m.momentums(energy, theta + dtheta).len() > i {
+                        theta += dtheta;
+                    } else {
+                        dtheta /= 2.0;
+                    }
+                }
+                new += (prev[i] - m.momentums(energy, theta)[i]).len() /
+                       m.energy_gradient(&prev[i]).len();
+            }
+
             prev = curr;
         }
         n *= 2;
